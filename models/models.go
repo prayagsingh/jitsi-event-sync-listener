50cd3260cf4e9fda6ce6b22bd3f22abeb3c99bda
prayagsingh <prayagsingh03@gmail.com>
2022-02-12 12:10:56 +0530

    fixed unixtimestamp issue

--- models/models.go
+++ models/models.go
@@ -1,21 +1,19 @@
 package models
 
-import "time"
-
 type userDetails struct {
 	Name        string `json:"name"`
 	Email       string `json:"email"`
 	Id          string `json:"id"`
 	OccupantJid string `json:"occupant_jid"`
-	JoinedAt    int    `json:"joined_at"`
-	LeftAt      int    `json:"left_at"`
+	JoinedAt    int64  `json:"joined_at"`
+	LeftAt      int64  `json:"left_at"`
 }
 
 type RequestData struct {
 	EventName    string        `json:"event_name"`
 	RoomName     string        `json:"room_name"`
 	RoomJID      string        `json:"room_jid"`
-	CreatedAt    time.Duration `json:"created_at"`
-	DestroyedAt  time.Duration `json:"destroyed_at"`
+	CreatedAt    int64         `json:"created_at"`
+	DestroyedAt  int64         `json:"destroyed_at"`
 	AllOccupants []userDetails `json:"all_occupants"`
 }
